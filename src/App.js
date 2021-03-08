16e2e1c32414d7d16382ef102c82f31e18b84bba
DonghoonKang <DonghoonKang@gangdonghun-ui-MacBook-Air.local>
2021-03-08 16:34:40 +0900

    Making Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
-import { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import TOC from './components/TOC'
+import Subject from './components/Subject'
+import Content from './components/Content'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
