35be27a5782f0d8787ffb2450d1f6371ad0a2dc2
donghoonKang-dev <donghoon.dev@gmail.com>
2021-03-11 01:19:33 +0900

    Event 1/3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
+      mode:'welcome',
       subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -17,6 +19,14 @@ class App extends Component {
     }
   }
   render() {
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title
+      _desc = this.state.welcome.desc
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
         <Subject 
