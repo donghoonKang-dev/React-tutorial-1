87d892935e5b388b768d1b7971d916c09cb7c05d
donghoonKang-dev <donghoon.dev@gmail.com>
2021-03-14 00:59:56 +0900

    Implement of Create Operation

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react'
 
 class TOC extends Component {
-    render() {
-        let lists = []
-        let data = this.props.data
-        let i = 0
-        while(i<data.length){
-            lists.push(
-            <li key={data[i].id}>
-              <a 
-                href={"/content/"+data[i].id}
-                data-id={data[i].id}
-                onClick={function(e){
-                  let id = e.target.dataset.id
-                  e.preventDefault()
-                  this.props.onChangePage(id)
-                }.bind(this)}>{data[i].title}
-              </a>
-            </li>)
-            i = i + 1;
-        }
-      return (
-        <nav>
-              <ul>
-                  {lists}
-              </ul>
-          </nav>
-      )
+  shouldComponentUpdate(newProps, newState){
+    if(newProps === newState){
+      return false
     }
+    return true
+  }
+  render() {
+      let lists = []
+      let data = this.props.data
+      let i = 0
+      while(i<data.length){
+          lists.push(
+          <li key={data[i].id}>
+            <a 
+              href={"/content/"+data[i].id}
+              data-id={data[i].id}
+              onClick={function(e){
+                let id = e.target.dataset.id
+                e.preventDefault()
+                this.props.onChangePage(id)
+              }.bind(this)}>{data[i].title}
+            </a>
+          </li>)
+          i = i + 1;
+      }
+    return (
+      <nav>
+            <ul>
+                {lists}
+            </ul>
+        </nav>
+    )
+  }
   }
 
   export default TOC
